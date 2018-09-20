c35082550911e9b1abb0cfff928a358329a9a5e0
Твой <vasvasal@gmail.com>
2018-09-20 21:31:57 +0500

    first commit

---
+++ Знакомство с жителями Сортлэнда/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Знакомство_с_жителями_Сортлэнда
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Секретарь Своп/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Секретарь_Своп
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
