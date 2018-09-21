5533b57de1a0c1ef9d9ca6918a1c25f72f4038ea
Твой <vasvasal@gmail.com>
2018-09-21 12:34:05 +0500

    a+b done

---
+++ a+b/a+b.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace a_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("INPUT.TXT");
+            string[] str = sr.ReadLine().Split(' ');
+            long a = long.Parse(str[0]);
+            long b = long.Parse(str[1]);
+            long result = a + b;
+            StreamWriter sw = new StreamWriter("OUTPUT.TXT");
+            sw.WriteLine(result.ToString());
+            sw.Dispose();
+            sr.Dispose();
+        }
+    }
+}
